19d41a62897e957b6880d8bba017feb800df970c
daniel.shih <daniel.shih@allpay.com.tw>
2017-08-02 15:33:21 +0800

    add ProxyPattern

---
+++ DesignPattern/ProxyPattern/ProxyFactroy.cs
@@ -0,0 +1,25 @@
+﻿using ProxyPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class ProxyFactroy<T> where T : class
+    {
+        private DynamicProxy<T> _proxy;
+        public ProxyFactroy(Type instance,object[] args)
+        {
+            var obj = Activator.CreateInstance(instance, args) as T;
+            _proxy = new DynamicProxy<T>(obj);
+        }
+
+        public T GetInstace()
+        {
+            return _proxy.GetTransparentProxy() as T;
+        }
+
+    }
+}
---
+++ DesignPattern/ProxyPattern/RealSubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    /// <summary>
+    /// 真正要執行的類別
+    /// </summary>
+    public class RealSubject : ISubject
+    {
+        public void Work()
+        {
+            Console.WriteLine("It is RealSubject");
+        }
+    }
+}
