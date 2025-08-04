ca698253af271ac1ef09c5412f6594996f01d974
danielshih <dog830228@gmail.com>
2025-08-04 14:23:46 +0800

    add iterator pattern rust example.

---
+++ Rust_DesignPattern/IteratorPattern/src/lib.rs
@@ -0,0 +1,37 @@
+
+struct IntegerIterator {
+    current: i32,
+    end: i32,
+}
+
+impl Iterator for IntegerIterator {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.end {
+            let value = self.current;
+            self.current += 1;
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_integer_iterator() {
+        let mut iter = IntegerIterator { current: 0, end: 5 };
+        iter.next(); // Move to the first element
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(4));
+        assert_eq!(iter.next(), None);
+    }
+
+
+}
